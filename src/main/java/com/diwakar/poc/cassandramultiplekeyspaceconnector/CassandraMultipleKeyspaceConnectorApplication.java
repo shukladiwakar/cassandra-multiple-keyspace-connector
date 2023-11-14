eab94ed54d1afe5b47bc493759717efc35ed80d9
Diwakar Shukla <diwakarshukla@192.168.1.2>
2023-11-14 19:02:01 +0530

    init

---
+++ src/main/java/com/diwakar/poc/cassandramultiplekeyspaceconnector/CassandraMultipleKeyspaceConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.diwakar.poc.cassandramultiplekeyspaceconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraMultipleKeyspaceConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CassandraMultipleKeyspaceConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diwakar/poc/cassandramultiplekeyspaceconnector/CassandraMultipleKeyspaceConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diwakar.poc.cassandramultiplekeyspaceconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraMultipleKeyspaceConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
